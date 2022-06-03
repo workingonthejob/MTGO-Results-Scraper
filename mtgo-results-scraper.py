c6556cda527c13e101e323bc2c32002d57ed01b6
workingonthejob <cs.masj@gmail.com>
2022-06-03 02:08:33 -0400

    Added argparse and the ability to screeenshot the decks.

--- mtgo-results-scraper.py
+++ mtgo-results-scraper.py
@@ -1,20 +1,35 @@
 import requests
+import argparse
+import sys
 from lxml import html
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium import webdriver
 from pprint import pprint
+
+
+DESCRIPTION = """
+Scrape and/or screenshot the Magic: The Gathering match results.
+"""
+TIMEOUT = 10
 
 
 class MTGOResultsScraper():
 
-    def __init__(self, url):
+    def __init__(self, url, output_dir, take_screenshots):
         self.url = url
+        self.output_dir = output_dir
+        self.take_screenshots = take_screenshots
         self.x_deck_container = '//div[@class="deck-group"]'
         self.x_player = './/h4'
         self.x_main_card_names ='.//div[@class="sorted-by-overview-container sortedContainer"]//span[@class="card-name"]/a'
         self.x_main_card_counts = './/div[@class="sorted-by-overview-container sortedContainer"]//span[@class="card-count"]'
         self.x_side_card_names = './/div[@class="sorted-by-sideboard-container  clearfix element"]//span[@class="card-name"]/a'
         self.x_side_card_counts = './/div[@class="sorted-by-sideboard-container  clearfix element"]//span[@class="card-count"]'
+        self.x_no_thanks_btn = '//button[@class="decline-button eu-cookie-compliance-default-button"]'
         self.headers = {'User-Agent':
                         'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                         'AppleWebKit/537.36 (KHTML, like Gecko)'
@@ -30,7 +45,7 @@ class MTGOResultsScraper():
         session.mount('https://', adapter)
         self.session = session.get(self.url, headers=self.headers)
 
-    def run(self):
+    def print_decklists(self):
         self.start_session()
         tree = html.fromstring(self.session.content)
         deck_containers = tree.xpath(self.x_deck_container)
@@ -51,7 +66,6 @@ class MTGOResultsScraper():
             for i in range(len(main_card_counts)):
                 number_of_cards = int(main_card_counts[i].text)
                 card_name = main_card_names[i].text
-                # print("{} {}".format(number_of_cards, card_name))
                 total_cards_main += number_of_cards
                 mainboard[card_name] = number_of_cards
 
@@ -60,14 +74,62 @@ class MTGOResultsScraper():
             for i in range(len(side_card_counts)):
                 number_of_cards = int(side_card_counts[i].text)
                 card_name = side_card_names[i].text
-                # print("{} {}".format(number_of_cards, card_name))
                 total_cards_side += number_of_cards
                 sideboard[card_name] = number_of_cards
             # print("{} total cards in sideboard.".format(total_cards_side))
             # print("----------------")
             pprint(container)
 
+    def take_decklist_screenshots(self):
+        # Define the options we want
+        # More options here:
+        # https://peter.sh/experiments/chromium-command-line-switches/
+        options = Options()
+        options.add_argument("--headless")
+        # Starting maximized headless doesn't work correctly.
+        options.add_argument("--window-size=1920x1080")
+        driver = webdriver.Chrome(options=options)
+
+        try:
+            driver.get(self.url)
+            driver.execute_script(
+                'document.querySelector("header").style.display = "none"')
+            wait = WebDriverWait(driver, TIMEOUT)
+            clickable = ec.element_to_be_clickable((By.XPATH, self.x_no_thanks_btn))
+            no_thanks_btn_elm = wait.until(clickable)
+            no_thanks_btn_elm.click()
+            decks = driver.find_elements(by=By.XPATH, value=self.x_deck_container)
+            names = driver.find_elements(by=By.XPATH, value=self.x_player)
+            number_of_decks = len(decks)
+
+            for i in range(number_of_decks):
+                player_name = names[i].text.split(" ")[0]
+                print("{}[{}/{}]".format(player_name, i + 1, number_of_decks))
+                decks[i].location_once_scrolled_into_view
+                decks[i].screenshot(r"{}\{}.png".format(self.output_dir, player_name))
+        except Exception as e:
+            print(e)
+        finally:
+            driver.quit()
+
+    def run(self):
+        if self.take_screenshots:
+            self.take_decklist_screenshots()
+
+        self.print_decklists()
+
 
 if __name__ == "__main__":
-    scraper = MTGOResultsScraper("http://magic.wizards.com/en/articles/archive/mtgo-standings/modern-league-2022-05-27")
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(0)
+    parser.add_argument("-o","--output-dir", help="The directory to save content to.", default=r".\"")
+    parser.add_argument("-s","--take-screenshots", help="Take screenshots of the decks.", action='store_true')
+    parser.add_argument("-u","--url", help="The page to start at or create screenshots of.", required=True)
+    args = parser.parse_args()
+
+    scraper = MTGOResultsScraper(args.url,
+                                 r"{}".format(args.output_dir),
+                                 args.take_screenshots)
     scraper.run()
