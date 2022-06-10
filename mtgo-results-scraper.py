831c4df0235fe31b8c6fa3dfb2e92c3b992cac60
workingonthejob <cs.masj@gmail.com>
2022-06-10 00:22:49 -0400

    Added folder creation for the screenshots.

--- mtgo-results-scraper.py
+++ mtgo-results-scraper.py
@@ -2,6 +2,7 @@ import requests
 import argparse
 import sys
 import os
+import re
 from lxml import html
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
@@ -21,10 +22,13 @@ TIMEOUT = 10
 
 class MTGOResultsScraper():
 
-    def __init__(self, url, output_dir, take_screenshots):
+    def __init__(self, url, output_dir, take_screenshots,
+        upload_to_imgur, export_to_markdown):
         self.url = url
         self.output_dir = output_dir
         self.take_screenshots = take_screenshots
+        self.upload_to_imgur = upload_to_imgur
+        self.export_to_markdown = export_to_markdown
         self.x_deck_container = '//div[@class="deck-group"]'
         self.x_player = './/h4'
         self.x_main_card_names ='.//div[@class="sorted-by-overview-container sortedContainer"]//span[@class="card-name"]/a'
@@ -32,6 +36,8 @@ class MTGOResultsScraper():
         self.x_side_card_names = './/div[@class="sorted-by-sideboard-container  clearfix element"]//span[@class="card-name"]/a'
         self.x_side_card_counts = './/div[@class="sorted-by-sideboard-container  clearfix element"]//span[@class="card-count"]'
         self.x_no_thanks_btn = '//button[@class="decline-button eu-cookie-compliance-default-button"]'
+        self.x_league_type = './/h1'
+        self.x_posted_in = './/div[@id="main-content"]//p[@class="posted-in"]'
         self.x_decklist_icons = './/span[@class="decklist-icons"]'
         self.headers = {'User-Agent':
                         'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
@@ -49,7 +55,6 @@ class MTGOResultsScraper():
         self.session = session.get(self.url, headers=self.headers)
 
     def print_decklists(self):
-        self.start_session()
         tree = html.fromstring(self.session.content)
         deck_containers = tree.xpath(self.x_deck_container)
         for deck in deck_containers:
@@ -110,7 +115,6 @@ class MTGOResultsScraper():
                 output_file = r"{}\{}-{}.png".format(self.output_dir, player_name, i)
                 print("{}[{}/{}]".format(player_name, i + 1, number_of_decks))
                 decks[i].location_once_scrolled_into_view
-                print(dir(decks[i]))
                 driver.execute_script(
                     'document.querySelectorAll("span.decklist-icons")[{}].style.display = "none"'.format(i))
                 decks[i].screenshot(output_file)
@@ -119,10 +123,26 @@ class MTGOResultsScraper():
         finally:
             driver.quit()
 
-    def run(self):
-        if self.take_screenshots:
-            self.take_decklist_screenshots()
+    def create_folder_for_screenshots(self):
+        tree = html.fromstring(self.session.content)
+        league = tree.find(self.x_league_type).text_content()
+        posted_in = tree.find(self.x_posted_in)
+        text = posted_in.text_content().strip()
+        date = re.search('\\w+\\s\\d{,2},\\s\\d{4}', text).group()
+        folder_name = "MTGO 5-0 {} Results ({})".format(league, date)
+        new_output_dir = "\\".join([self.output_dir, folder_name])
+        os.makedirs(new_output_dir, exist_ok=True)
+        self.output_dir = new_output_dir
 
+    def run(self):
+        self.start_session()
+        if self.take_screenshots:
+            self.create_folder_for_screenshots()
+            self.take_decklist_screenshots()
+        if self.take_screenshots and self.upload_to_imgur:
+            pass
+        if self.export_to_markdown:
+            pass
         self.print_decklists()
 
 
@@ -134,11 +154,13 @@ if __name__ == "__main__":
     parser.add_argument("-o","--output-dir", help="The directory to save content to.", default=".")
     parser.add_argument("-s","--take-screenshots", help="Take screenshots of the decks.", action='store_true')
     parser.add_argument("-u","--url", help="The page to start at or create screenshots of.", required=True)
-    parser.add_argument("-i","--upload-to-imgur", help="Create an Imgur album and upload deck images to it.")
-    parser.add_argument("-e","--export-to-markdown", help="Export the deck images to markdown for reddit.")
+    parser.add_argument("-i","--upload-to-imgur", help="Create an Imgur album and upload deck images to it.", action='store_false')
+    parser.add_argument("-e","--export-to-markdown", help="Export the deck images to markdown for reddit.", action='store_false')
     args = parser.parse_args()
 
     scraper = MTGOResultsScraper(args.url,
                                  r"{}".format(args.output_dir),
-                                 args.take_screenshots)
+                                 args.take_screenshots,
+                                 args.upload_to_imgur,
+                                 args.export_to_markdown)
     scraper.run()
