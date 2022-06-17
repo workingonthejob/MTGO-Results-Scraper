0f40e00055bb10df42cd3c2e84147613157582b2
workingonthejob <cs.masj@gmail.com>
2022-06-17 15:46:01 -0400

    Added latest set from mtgjson, added parser for json, and generated the text file for those cards. #1

---
+++ tools/mtg_json_parser.py
@@ -0,0 +1,23 @@
+import json
+import glob
+
+exclude = ("Plains", "Swamp", "Island", "Forest", "Mountain")
+
+
+def parse_mtg_json_for_latest_cards():
+    card_tracker = []
+    file = glob.glob('../resources/*.json')[0]
+    with open(file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        with open(r'..\resources\latest_cards.txt', 'w', encoding='utf-8') as handle:
+            for card in data['data']['cards']:
+                card_name = card['name']
+                # Remove duplicates and exclude specific cards
+                if card_name not in card_tracker and card_name not in exclude:
+                    handle.write(card_name + '\n')
+                    card_tracker.append(card_name)
+            # Remove newline at EOF
+            handle.truncate(handle.tell() - 2)
+
+
+parse_mtg_json_for_latest_cards()
