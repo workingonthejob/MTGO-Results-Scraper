394f57faaf82cea07bdaf07b2cf04764f1a216b2
workingonthejob <cs.masj@gmail.com>
2022-06-20 22:10:55 -0400

    Updated to requirements and chromedriver usage.

--- src/mtgo-results-scraper.py
+++ src/mtgo-results-scraper.py
@@ -13,6 +13,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from pprint import pprint
 from imgur import Imgur
@@ -112,7 +114,8 @@ class MTGOResultsScraper():
         options.add_argument("--headless")
         # Starting maximized headless doesn't work correctly.
         options.add_argument("--window-size=1920x1080")
-        self.driver = webdriver.Chrome(options=options)
+        self.driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=options)
         self.driver.get(self.url)
 
     def take_decklist_screenshots(self):
