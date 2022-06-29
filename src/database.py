8e8423b01bcee8f077b54d7ed2c456d46d1c2dfc
workingonthejob <cs.masj@gmail.com>
2022-06-29 00:37:56 -0400

    Preliminary work for DB and removed record date stuff.

--- src/database.py
+++ src/database.py
@@ -1,40 +1,55 @@
 import sqlite3
+import logging
+from logging.config import fileConfig
+
+
+fileConfig('logging_config.ini')
+log = logging.getLogger()
 
 
 class Database():
 
     def __init__(self, name):
-        self.db = sqlite3.connect("{}.sqlite3".format(name))
-        self.tables = ["modern_results", "pioneer_results"]
+        self.db = sqlite3.connect("{}.sqlite3".format(
+            name.split('.sqlite3')[0]))
+        self.tables = ["imgur_uploads"]
         self.cursor = self.db.cursor()
-        self._create_hashes_table()
+        self._create_imgur_tables()
 
-    def _create_hashes_table(self):
+    def _create_imgur_tables(self):
         for table in self.tables:
             self.cursor.execute("CREATE TABLE IF NOT EXISTS "
                                 "{}("
                                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"
                                 "file text,"
-                                "hash text)"
+                                "album_id text,"
+                                "player text,"
+                                "url text)"
                                 .format(table))
 
     def update_row(self):
         # https://www.sqlitetutorial.net/sqlite-update/
         pass
 
-    def add_row(self, file, md5):
+    def add_row(self, file, album_id, player, url):
         self.cursor.execute("INSERT INTO "
                             "{}("
                             "file,"
-                            "hash) "
-                            "VALUES (?, ?)"
-                            .format(self.table),
-                            ('{}'.format(file),
-                             '{}'.format(md5)))
+                            "album_id,"
+                            "player,"
+                            "url) "
+                            "VALUES (?, ?, ?, ?)"
+                            .format('imgur_uploads'),
+                            (file,
+                             album_id,
+                             player,
+                             url))
         self.commit()
 
-    def is_hash_in_table(self, value):
-        return self.column_contains(self.table, "hash", value)
+    def delete_file(self, file):
+        self.cursor.execute("DELETE FROM {} WHERE file = '{}'".format(
+            'imgur_uploads', file))
+        self.commit()
 
     def is_file_in_table(self, value):
         return self.column_contains(self.table, "file", value)
@@ -59,3 +74,15 @@ class Database():
     def close(self):
         log.debug("Closing database connection.")
         self.db.close()
+
+
+if __name__ == "__main__":
+    db = None
+    try:
+        db = Database("Uploads")
+        # db.add_row(r'C:\foo\smthing.png', 'xYfk29', 'Player_Name', 'https://something.com')
+        db.delete_file(r'C:\foo\smthing.png')
+    except Exception as e:
+        log.exception(e)
+    finally:
+        db.close()
--- src/results_page_checker.py
+++ src/results_page_checker.py
@@ -28,16 +28,10 @@ MODERN_LEAGUE_LINK = BASE_URL + "modern-league-{}".format(TODAY)
 MODERN_CHALLENGE_LINK = BASE_URL + "modern-challenge-{}".format(TODAY)
 TEST_LINK = BASE_URL + "modern-league-{}".format('2022-06-17')
 LINKS = [PIONEER_LEAGUE_LINK,
-         PIONEER_CHALLENGE_LINK, TEST_LINK]
+         PIONEER_CHALLENGE_LINK]
 # xpath
 X_NO_RESULT = './/p[@class="no-result"]'
 ALREADY_PROCESSED_LINKS = []
-LAST_CHECKED_DATE = {
-    'pioneer_league': None,
-    'pioneer_challenge': None,
-    'modern_league': None,
-    'modern_challenge': None
-}
 
 
 class Checker():
@@ -58,20 +52,6 @@ class Checker():
         self.session.mount('http://', adapter)
         self.session.mount('https://', adapter)
 
-    def record_date(self, link):
-        if link == PIONEER_LEAGUE_LINK:
-            print('Pioneer League')
-            LAST_CHECKED_DATE['pioneer_league'] = TODAY
-        elif link == PIONEER_CHALLENGE_LINK:
-            print('Pioneer Challenge')
-            LAST_CHECKED_DATE['pioneer_challenge'] = TODAY
-        elif link == MODERN_LEAGUE_LINK:
-            print('Modern League')
-            LAST_CHECKED_DATE['modern_league'] = TODAY
-        elif link == MODERN_CHALLENGE_LINK:
-            print('Modern Challenge')
-            LAST_CHECKED_DATE['modern_challenge'] = TODAY
-
     def run(self):
         self.start_session()
 
@@ -84,7 +64,6 @@ class Checker():
                     tree = html.fromstring(s.content)
                     results = tree.find(X_NO_RESULT)
                     if results is None and link not in ALREADY_PROCESSED_LINKS:
-                        self.record_date(link)
                         mrs = MTGOResultsScraper(link,
                                                  OUTPUT_DIRECTORY,
                                                  TAKE_SCREENSHOTS,
@@ -96,10 +75,11 @@ class Checker():
                         folder_name = mrs.get_folder_name()
                         im = Imgur()
                         for screenshot in mrs.get_screenshots():
+                            screenshot_file = screenshot['screenshot']['file']
                             log.info("Uploading screenshot {}".format(
-                                screenshot))
+                                screenshot_file))
                             # For every 50 screenshot uploads sleep for an hour
-                            if not screenshot_count % 50:
+                            if not screenshot_count % 50 and screenshot_count > 0:
                                 time_elapsed = 0
                                 log.info('Imgur API upload limit reached.')
                                 # Update user every minute
@@ -108,8 +88,8 @@ class Checker():
                                     log.info('Slept for {} minutes so far.'.format(time_elapsed_min))
                                     time.sleep(60)
                                     time_elapsed += 60
-                            album_id = im.create_album(title=folder_name)
-                            im.upload_image(image=screenshot, album=album_id)
+                            album_id = im.create_album(title=folder_name)['data']['id']
+                            im.upload_image(image=screenshot_file, album=album_id)
                             screenshot_count += 1
                         ALREADY_PROCESSED_LINKS.append(link)
                 except KeyboardInterrupt as e:
